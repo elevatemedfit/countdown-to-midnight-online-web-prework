00e1d06bfad74b1464686e9fbd836cb9f50e239d
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-19 14:04:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,32 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  while time>0
+    puts  "#{time} SECOND(S)!"
+    time-=1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+# countdown(12)
+# def countdown(time)
+#   while time>0
+#     puts  "#{time} SECOND(S)!"
+#     time-=1
+#   end
+#
+# end
+
+# countdown(12)
+
+
+
+
+
+def countdown_with_sleep(time)
+  while time>0
+    time-=1
+    sleep 1
+  end
+end
+# countdown_with_sleep(10)
